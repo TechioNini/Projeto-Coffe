141480527c705730e1acec31317869fa9d992c8f
Rony <102264786+ronyfenrir@users.noreply.github.com>
2024-01-11 09:53:40 -0300

    atualização indeex

--- indeex.js
+++ indeex.js
@@ -1,1 +1,32 @@
+const express = require("express");
+const app = express();
 
+app.get("/", function(req, res){
+    res.send("Seja Bem vindo ao meu app");
+});
+
+app.get("/Home", function(req, res){
+    res.send("bem vindo ao Home");
+});
+
+app.get("/About", function(req, res){
+    res.send("Pagina About");
+});
+
+app.get("/Coffee", function(req, res){
+    res.send("Pagina Coffee");
+});
+
+
+app.get("/Menu", function(req, res){
+    res.send("Pagina Menu");
+});
+
+app.get("/Login", function(req, res){
+    res.send("Pagina Login");
+});
+
+
+
+// todos os cod acima desta linha Localhost:8080
+app.listen(8080);
